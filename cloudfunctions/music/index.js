2d6ea513696054b49434417c199e5cfe6a0700db
APitaya1 <1842299097@qq.com>
2021-01-30 23:42:36 +0800

    day6

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -42,6 +42,7 @@ exports.main = async (event, context) => {
   //根据歌单id获取歌曲播放的url
   app.router('musicUrl',async(ctx,next)=>{
     const res=await axios.get(`${BASE_URL}/song/url?id=${event.musicId}`)
+    ctx.body=res.data
   })
   return app.serve()
 }
--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,11 +1,12 @@
-// pages/player/player.js
+let musicList = []
+//正在播放的歌曲index
+let playingIndex = 0
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    picUrl: ''
   },
 
   /**
@@ -13,55 +14,29 @@ Page({
    */
   onLoad: function (options) {
     console.log(options)
-    console.log(options.musicId,typeof(options.musicId))
+    console.log(options.musicId, typeof (options.musicId))
+    playingIndex = options.index
+    musicList = wx.getStorageSync('musiclist')
+    this._loadMusicDetail(options.musicId)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  _loadMusicDetail(musicId) {
+    let music = musicList[playingIndex]
+    console.log(music)
+    wx.setNavigationBarTitle({
+      title: music.name,
+    })
+    this.setData({
+      picUrl: music.al.picUrl
+    })
+    wx.cloud.callFunction({
+      name: 'music',
+      data: {
+        $url: 'musicUrl',
+        musicId: musicId
+      }
+    }).then((res) => {
+      console.log(res)
+    })
   }
 })
